fixed camera rotation direction

src/display_baxter_trajectory/scripts/display_left_arm_npy.py
@@ -60,6 +60,12 @@ class DisplayBaxterTrajectory():
 
         self.tf_br_camera2baxter = tf.TransformBroadcaster()
 
+        # init_cam_mat_x90z90 = np.matmul(self.doRotation3DInputAngle(np.pi / 2, 0), self.doRotation3DInputAngle(np.pi / 2, 2))
+        # self.init_cam_mat = init_cam_mat_x90z90.copy()
+        # self.init_cam_mat[:, -1] = -1 * init_cam_mat_x90z90[:, -1]
+
+        # pdb.set_trace()
+
     def publishJointStates(self, frame_id):
         # Construct message & publish joint states
 
@@ -72,18 +78,58 @@ class DisplayBaxterTrajectory():
 
     def broadTFCameraToBaxterBase(self, frame_id):
 
+        self.tf_br_camera2baxter.sendTransform(
+            (self.cam2baxter_traj_npy[frame_id, 0], self.cam2baxter_traj_npy[frame_id, 1], self.cam2baxter_traj_npy[frame_id, 2]),
+            (self.cam2baxter_traj_npy[frame_id, 3], self.cam2baxter_traj_npy[frame_id, 4], self.cam2baxter_traj_npy[frame_id, 5], self.cam2baxter_traj_npy[frame_id, 6]), rospy.Time.now(),
+            'opencv_camera_base', 'left_arm_mount')
+
         # self.cam2baxter_tvec = self.cam2baxter_traj_npy[frame_id, 0:3]
 
-        cam2baxter_quat = self.cam2baxter_traj_npy[frame_id, 3:-1]
+        # cam2baxter_quat = self.cam2baxter_traj_npy[frame_id, 3:7]
+        # cam2baxter_mat = self.getInputRotationMat(cam2baxter_quat)
 
-        # self.tf_br_camera2baxter.sendTransform(
-        #     (self.cam2baxter_traj_npy[frame_id, 0], self.cam2baxter_traj_npy[frame_id, 1], self.cam2baxter_traj_npy[frame_id, 2]),
-        #     (self.cam2baxter_traj_npy[frame_id, 3], self.cam2baxter_traj_npy[frame_id, 4], self.cam2baxter_traj_npy[frame_id, 5], self.cam2baxter_traj_npy[frame_id, 6]), rospy.Time.now(),
-        #     'camera_base', 'left_arm_mount')
+        # self.init_cam_mat = np.array([[0.0000000, -0.0000000, 1.0000000], [1.0000000, 0.0000000, -0.0000000], [0.0000000, 1.0000000, 0.0000000]])
+        # rotated_cam2baxter_mat = np.matmul(cam2baxter_mat, self.init_cam_mat)
 
-        self.tf_br_camera2baxter.sendTransform((self.cam2baxter_traj_npy[frame_id, 0], self.cam2baxter_traj_npy[frame_id, 1], self.cam2baxter_traj_npy[frame_id, 2]), (0, 0, 0, 1), rospy.Time.now(),
-                                               'camera_base', 'left_arm_mount')
+        # rotated_cam2baxter_mat_H = np.vstack((np.hstack((rotated_cam2baxter_mat, np.array([[0], [0], [0]]))), np.array([0, 0, 0, 1])))
+        # rotated_cam2baxter_quat = tf.transformations.quaternion_from_matrix(rotated_cam2baxter_mat_H)
+
+        # self.tf_br_camera2baxter.sendTransform((self.cam2baxter_traj_npy[frame_id, 0], self.cam2baxter_traj_npy[frame_id, 1], self.cam2baxter_traj_npy[frame_id, 2]), list(rotated_cam2baxter_quat),
+        #                                        rospy.Time.now(), 'camera_base', 'left_arm_mount')
+
+        # (alpha, beta, gammaa) = tf.transformations.euler_from_quaternion(cam2baxter_quat)
+        # rotated_cam2baxter_quat = tf.transformations.quaternion_from_euler(alpha, beta, gammaa)
+        # self.tf_br_camera2baxter.sendTransform((self.cam2baxter_traj_npy[frame_id, 0], self.cam2baxter_traj_npy[frame_id, 1], self.cam2baxter_traj_npy[frame_id, 2]),
+        #                                        (rotated_cam2baxter_quat[0], rotated_cam2baxter_quat[1], rotated_cam2baxter_quat[2], rotated_cam2baxter_quat[3]), rospy.Time.now(), 'opencv_cam_base',
+        #                                        'left_arm_mount')
+
+        self.tf_br_camera2baxter.sendTransform((0, 0, 0), (-0.5, 0.5, 0.5, 0.5), rospy.Time.now(), 'urdf_camera_base', 'opencv_camera_base')
+
+        # self.tf_br_camera2baxter.sendTransform((self.cam2baxter_traj_npy[frame_id, 0], self.cam2baxter_traj_npy[frame_id, 1], self.cam2baxter_traj_npy[frame_id, 2]), (0, 0, 0, 1), rospy.Time.now(),
+        #                                        'camera_base', 'left_arm_mount')
         # self.tf_br_camera2baxter.sendTransform((0, 0, 0), (0, 0, 0, 1), rospy.Time.now(), 'camera_base', 'left_arm_mount')
+
+    def getInputRotationMat(self, input_pose):
+        return transforms3d.quaternions.quat2mat([input_pose[3], input_pose[0], input_pose[1], input_pose[2]])
+
+    def doRotation3DInputAngle(self, angle, axis=0):
+        # points: [N, point_size, 3]
+        # return rotation matrix per axis : counter-clockwise?
+        rot_sin = np.sin(angle)
+        rot_cos = np.cos(angle)
+        if axis == 0:
+            rot_mat = np.stack([
+                [1, 0, 0],
+                [0, rot_cos, -rot_sin],
+                [0, rot_sin, rot_cos],
+            ])
+        elif axis == 1:
+            rot_mat = np.stack([[rot_cos, 0, rot_sin], [0, 1, 0], [-rot_sin, 0, rot_cos]])
+        elif axis == 2:
+            rot_mat = np.stack([[rot_cos, -rot_sin, 0], [rot_sin, rot_cos, 0], [0, 0, 1]])
+        else:
+            raise ValueError("axis should in range")
+        return rot_mat
 
     def shutdown(self):
         # sys.exit()
@@ -119,7 +165,7 @@ if __name__ == '__main__':
             if frame_id >= demoLeftArm.left_arm_traj_npy.shape[0]:
                 break
 
-            rospy.sleep(0.1)
+            rospy.sleep(1.1)
 
     except KeyboardInterrupt:
         print('error!')